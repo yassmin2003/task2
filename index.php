9e7a207a277e5573a2f64327b98d17bf16426170
Yasmine Hamed <154928731+yassmin2003@users.noreply.github.com>
2025-04-22 04:58:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,4 +13,20 @@ function arr($names){
 }
 
 print_r( arr(["ahmed","ali","osama","ramadan","mahmoud","eslam"]));
+print("<br>");
+
+//or////////////////////////////////////////////////
+
+function arr2($names){
+
+    foreach($names as $newarray){ // newarray of i = names of i for each iterarion passes array to other
+        if(strlen($names)!=5){
+            continue;
+        }
+        
+    }
+    return $newarray;
+
+}
+print_r( arr(["ahmed","ali","osama","ramadan","mahmoud","eslam"]));
 ?>
