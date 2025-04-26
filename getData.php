9a4f245c42b164f72694a01380b93b4666583201
Yasmine Hamed <154928731+yassmin2003@users.noreply.github.com>
2025-04-26 03:29:24 +0300

    Update getData.php

--- getData.php
+++ getData.php
@@ -1,1 +1,12 @@
+<?php
+//TASK 4
 
+$name=$_POST['username']; // REQUEST FOR BOTH GET AND POST
+    $password=$_POST['password'];
+    $email=$_POST['email'];
+
+    echo "Hello $name your email is $email and your password is $password";
+
+
+
+?>
